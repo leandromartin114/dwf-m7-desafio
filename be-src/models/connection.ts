7722fa007e40d88d60769c1f4ec0846ea0d567a3
Leandro <leandrom.roldan@gmail.com>
2022-08-03 13:00:43 -0300

    changes on env var on connection.ts

--- be-src/models/connection.ts
+++ be-src/models/connection.ts
@@ -1,17 +1,17 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
-const username = process.env.SEQUELIZE_CREDS_USERNAME;
-const password = process.env.SEQUELIZE_CREDS_PASSWORD;
-const database = process.env.SEQUELIZE_CREDS_DATABASE;
-const host = process.env.SEQUELIZE_CREDS_HOST;
+// const username = process.env.SEQUELIZE_CREDS_USERNAME;
+// const password = process.env.SEQUELIZE_CREDS_PASSWORD;
+// const database = process.env.SEQUELIZE_CREDS_DATABASE;
+// const host = process.env.SEQUELIZE_CREDS_HOST;
 
 export const sequelize = new Sequelize({
 	dialect: "postgres",
-	username: username.toString(),
-	password: password.toString(),
-	database: database.toString(),
+	username: process.env.SEQUELIZE_CREDS_USERNAME,
+	password: process.env.SEQUELIZE_CREDS_PASSWORD,
+	database: process.env.SEQUELIZE_CREDS_DATABASE,
 	port: 5432,
-	host: host.toString(),
+	host: process.env.SEQUELIZE_CREDS_HOST,
 	ssl: true,
 	dialectOptions: {
 		ssl: {
