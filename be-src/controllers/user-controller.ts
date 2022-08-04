818dacae947e75428b280ad9ca539ec23aa8151b
Leandro <leandrom.roldan@gmail.com>
2022-08-04 14:12:31 -0300

    adding trycatch into user-controller

--- be-src/controllers/user-controller.ts
+++ be-src/controllers/user-controller.ts
@@ -18,50 +18,61 @@ function getSHA256ofString(text) {
 export async function findUser(data) {
 	const userFinded = await User.findOne({
 		where: { email: data.email },
+	}).catch((error) => {
+		console.error(error);
 	});
-	if (userFinded) {
-		return userFinded;
-	} else {
-		console.error("User don't exist");
-	}
+
+	return userFinded;
 }
 //create user and his auth element if it doesn't exist
 export async function findOrCreateUser(bodyData) {
 	const { email, fullName, password } = bodyData;
-	const [user, created] = await User.findOrCreate({
-		where: { email: email },
-		defaults: {
-			email,
-			fullName,
-		},
-	});
-	const [auth, authCreated] = await Auth.findOrCreate({
-		where: { userId: user.get("id") },
-		defaults: {
-			email,
-			password: getSHA256ofString(password),
-			userId: user.get("id"),
-		},
-	});
-	return user;
+	try {
+		const [user, created] = await User.findOrCreate({
+			where: { email: email },
+			defaults: {
+				email,
+				fullName,
+			},
+		});
+		const [auth, authCreated] = await Auth.findOrCreate({
+			where: { userId: user.get("id") },
+			defaults: {
+				email,
+				password: getSHA256ofString(password),
+				userId: user.get("id"),
+			},
+		});
+		return user;
+	} catch (error) {
+		console.log(error);
+	}
 }
 //generate the token for validation
 export async function generateToken(bodyData) {
 	const { email, password } = bodyData;
 	const passwordHash = getSHA256ofString(password);
-	const auth = await Auth.findOne({
-		where: {
-			email,
-			password: passwordHash,
-		},
-	});
-	const token = jwt.sign({ id: auth.get("userId") }, SECRET);
-	return token;
+	try {
+		const auth = await Auth.findOne({
+			where: {
+				email,
+				password: passwordHash,
+			},
+		});
+		const token = jwt.sign({ id: auth.get("userId") }, SECRET);
+		return token;
+	} catch (error) {
+		console.log(error);
+	}
 }
 //getting the user data
 export async function getUserData(userId: number) {
-	const user = await User.findByPk(userId);
-	return user;
+	try {
+		const user = await User.findByPk(userId);
+		return user;
+	} catch (error) {
+		console.log(error);
+	}
 }
 //updating a user
 export async function updateUser(userId: number, data) {
@@ -75,11 +86,15 @@ export async function updateUser(userId: number, data) {
 		email: data.email,
 		password: passwordHash,
 	};
-	const userUpdated = await User.update(dataForUser, {
-		where: { id: userId },
-	});
-	const authUpdated = await Auth.update(dataForAuth, {
-		where: { userId: userId },
-	});
-	return userUpdated;
+	try {
+		const userUpdated = await User.update(dataForUser, {
+			where: { id: userId },
+		});
+		const authUpdated = await Auth.update(dataForAuth, {
+			where: { userId: userId },
+		});
+		return userUpdated;
+	} catch (error) {
+		console.log(error);
+	}
 }
