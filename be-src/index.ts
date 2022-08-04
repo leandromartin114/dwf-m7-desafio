ab31d952d55928bcfa5aa02bfca7cc910f31a895
Leandro <leandrom.roldan@gmail.com>
2022-08-04 13:54:11 -0300

    adding returns endpoints

--- be-src/index.ts
+++ be-src/index.ts
@@ -49,23 +49,23 @@ app.use(cors());
 //find user for signin
 app.post("/auth", async (req, res) => {
 	const existingUser = await findUser(req.body).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(existingUser);
+	return res.status(200).json(existingUser);
 });
 //signup
 app.post("/signup", async (req, res) => {
 	const user = await findOrCreateUser(req.body).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(user);
+	return res.status(200).json(user);
 });
 //signin
 app.post("/auth/token", async (req, res) => {
 	const token = await generateToken(req.body).catch((error) => {
-		res.status(401).json("email or pass incorrect " + error);
+		return res.status(401).json("email or pass incorrect " + error);
 	});
-	res.status(200).json(token);
+	return res.status(200).json(token);
 });
 //authorization midleware
 function authMid(req, res, next) {
@@ -75,59 +75,59 @@ function authMid(req, res, next) {
 		req._user = data;
 		next();
 	} catch (error) {
-		res.status(401).json(error);
+		return res.status(401).json(error);
 	}
 }
 //getting the user data with the mid function
 app.get("/user", authMid, async (req, res) => {
 	const user = await getUserData(req._user.id).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(user);
+	return res.status(200).json(user);
 });
 //update user data
 app.put("/user/update", authMid, async (req, res) => {
 	const userUpdated = await updateUser(req._user.id, req.body).catch(
 		(error) => {
-			res.status(400).json(error);
+			return res.status(400).json(error);
 		}
 	);
-	res.status(200).json(userUpdated);
+	return res.status(200).json(userUpdated);
 });
 //report new pet
 app.post("/pet/new", authMid, async (req, res) => {
 	const newPet = await createPet(req._user.id, req.body).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(newPet);
+	return res.status(200).json(newPet);
 });
 //edit pet
 app.put("/pet/update/:id", authMid, async (req, res) => {
 	const petUpdated = await uptadePet(req.params.id, req.body).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(petUpdated);
+	return res.status(200).json(petUpdated);
 });
 //get all my pets
 app.get("/pets", authMid, async (req, res) => {
 	const myPetsReported = await getAllMyPets(req._user.id).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(myPetsReported);
+	return res.status(200).json(myPetsReported);
 });
 //get pets near a location
 app.get("/pets-near-by", async (req, res) => {
 	const petsFinded = await getPetsNearBy(req.query).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(petsFinded);
+	return res.status(200).json(petsFinded);
 });
 //create report about a pet
 app.post("/report/:id", async (req, res) => {
 	const report = await createReport(req.params.id, req.body).catch((error) => {
-		res.status(400).json(error);
+		return res.status(400).json(error);
 	});
-	res.status(200).json(report);
+	return res.status(200).json(report);
 });
 
 //listen
