61069b57cc39bfc6ab132d27a939a38d59f036e2
Leandro <leandrom.roldan@gmail.com>
2022-08-04 17:56:06 -0300

    litle fix in user-controller and the env var api_base_url

--- be-src/controllers/user-controller.ts
+++ be-src/controllers/user-controller.ts
@@ -16,18 +16,14 @@ function getSHA256ofString(text) {
 
 //find user by email
 export async function findUser(data) {
-	// try {
-	// 	const userFinded = await User.findOne({
-	// 		where: { email: data.email },
-	// 	});
-	// 	return userFinded;
-	// } catch (error) {
-	// 	console.log(error);
-	// }
-	const userFinded = await User.findOne({
-		where: { email: data.email },
-	});
-	return userFinded;
+	try {
+		const userFinded = await User.findOne({
+			where: { email: data.email },
+		});
+		return userFinded;
+	} catch (error) {
+		console.log(error);
+	}
 }
 //create user and his auth element if it doesn't exist
 export async function findOrCreateUser(bodyData) {
